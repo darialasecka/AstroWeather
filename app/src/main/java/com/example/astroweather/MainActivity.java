670917f8007bf18416362d1f0d07debe92e3d194
darialasecka <daria.las@wp.pl>
2020-05-04 14:21:33 +0200

    Added in main activity fields to input coordinates and refresh time

--- app/src/main/java/com/example/astroweather/MainActivity.java
+++ app/src/main/java/com/example/astroweather/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.astroweather;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = findViewById(R.id.refresh_time);
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("5 minutes");
+        arrayList.add("10 minutes");
+        arrayList.add("15 minutes");
+        arrayList.add("20 minutes");
+        arrayList.add("25 minutes");
+        arrayList.add("30 minutes");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, arrayList);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(arrayAdapter);
+
+        /*
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                string tutorialsName = parent.getItemAtPosition(position).toString();
+                Toast.makeText(parent.getContext(), "Selected: " + tutorialsName, Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView <?> parent) {
+            }
+        });*/
     }
 }
